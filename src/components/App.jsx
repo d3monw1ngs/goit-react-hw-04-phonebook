3805d35ed32870b8e168a8432f4a41ed8e14f936
d3monw1ngs <tezzielumsit87@gmail.com>
2024-06-25 22:49:15 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,10 @@ const initialContacts = [
     ];
 
 export const App = () => {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},]);
 
   // Load contacts from local storage when the component mounts
   useEffect(() => {
